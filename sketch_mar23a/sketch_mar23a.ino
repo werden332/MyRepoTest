733629899eba9aa1479a1dae33f9f999d395600b
werden332 <108885886+werden332@users.noreply.github.com>
2026-03-24 14:20:43 +0300

    Старт

---
+++ sketch_mar23a/sketch_mar23a.ino
@@ -0,0 +1,36 @@
+#include <DHT.h> 
+int humid_temp = 2; //1
+int fan = 7; //1
+int humid_soil = A1; //1
+int heater = 4; //1
+int light = 6; //1
+int pump = 5; //1
+int light_data = A0;
+
+DHT dht(humid_temp, DHT11);
+unsigned long previousMillis = 0;
+void setup() {             
+  Serial.begin(9600);
+  dht.begin(); 
+  pinMode(fan, OUTPUT);
+  pinMode(heater, OUTPUT);
+  pinMode(light, OUTPUT);
+  pinMode(pump, OUTPUT);
+
+
+}
+
+void loop() {
+   unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= INTERVAL) {
+    previousMillis = currentMillis;
+  Serial.print("Humidity air: "); 
+  Serial.println(dht.readHumidity());
+  Serial.print("Temperature air: ");
+  Serial.println(dht.readTemperature());
+  Serial.print("Humidity soil: ");
+  Serial.println(analogRead(humid_soil));
+  Serial.print("Light sensor: ");
+  Serial.println(analogRead(light_data));
+  
+}
